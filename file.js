9c6944005fc2cdcbde6d1a68354488124b86ac63
abubakarm166 <143324758+abubakarm166@users.noreply.github.com>
2023-10-11 19:27:58 +0500

    Add files via upload

--- file.js
+++ file.js
@@ -5,21 +5,91 @@ const reven = document.getElementById("reven");
 const sel = document.getElementById("sel");
 const shift= document.getElementById("time")
 const re = document.getElementById("re");
+var shiftlen; 
 var io;
 var is;
 var ww;
 var ll;
 var op;
 var po;
+var wage;
+var EPTK;
+var savings;
+if (sel.value==="Alabama") {
+    wage=34.03;
+}
+else if (sel.value==="Alaska") {
+    wage=67.67;
+}
+else if (sel.value==="Arizona") {
+    wage=52.69;
+} 
+    else if (sel.value==="Arkansas") {
+        wage=51.11;
+    }
+    else if (sel.value==="California") {
+        wage=67.11;
+    }
+    else if (sel.value==="Colorado") {
+        wage=53.20;
+    }
+    else if (sel.value==="Connecticut") {
+        wage=52.48;
+    }    
+    else if (sel.value==="Delaware") {
+        wage=50.95;
+    }
+    else if (sel.value==="District") {
+        wage=66.57;
+    }
+    else if (sel.value==="Florida") {
+        wage=41.62;
+    }
+    else if (sel.value==="Georgia") {
+        wage=50.39;
+    }
+    
+    if (shift.value==="1") {
+        shiftlen=1;
+    } else if (shift.value==="2") {
+        shiftlen=2;
+    } 
+    else if (shift.value==="3") {
+    shiftlen=3;
+} 
+else if (shift.value==="4") {
+    shiftlen=4;
+}  else if (shift.value==="5") {
+    shiftlen=5;
+}  else if (shift.value==="6") {
+    shiftlen=6;
+        
+}  else if (shift.value==="7") {
+    shiftlen=7;
+}  else if (shift.value==="8") {
+    shiftlen=8;
+}  else if (shift.value==="9") {
+    shiftlen=9;
+} 
+else if (shift.value==="10") {
+    shiftlen=10;
+}  else if (shift.value==="11") {
+    shiftlen=11;
+}  else if (shift.value==="12") {
+    shiftlen=12;
+} 
 function ff() {
- if (sel.value==="Alabama" && input1.value< 940000) {
+
+
+ if (input1.value< 940000) {
 
 
 
 //    (totalAnnualAbsences * EDHDPV) * -1    
    
     io=input1.value;
-    is=Math.round((Number(io)*0.25)/208);
+    // EDHDPV =is
+    is=(Number(io)*0.25)/208;
     
 
     // ch
@@ -34,12 +104,15 @@ function ff() {
    po=Math.round(ll * is);
    reven.innerHTML=po;
     //Estimated Savings
-
-    // op= po-
-
+    //EPTK = shift * (WBS + (WBS * 0.3)) * totalAnnualAbsences
+    EPTK =Number(shiftlen)*(((Number(wage)+(Number(wage) * 0.3)))*Number(ll));
+    // input.innerHTML=EPTK; 
+     savings=(ll* is)-EPTK;
+    re.innerHTML=Math.round(savings);
+    if (savings<=0) {
+        re.innerHTML=0;
+    }90000
     }
-else{
-    reven.innerHTML === "0$";
-};
+
 
 }
